41008e74f5849d8a344bfaed4a93e3bd6ab150eb
Lindseyp314 <lindseyp314@ucla.edu>
2022-09-13 21:41:55 -0600

    started integrating quiz and map

--- src/components/data-lecture-halls/lecture-hall-game.js
+++ src/components/data-lecture-halls/lecture-hall-game.js
@@ -16,18 +16,21 @@ const MyMap = () => {
   const [lat, setLat] = useState(34.07162238659867);
   const [long, setLong] = useState(-118.44415583155065);
   const [zoom, setZoom] = useState(16);
+  const [correct, setCorrect] = useState(new Array(10).fill(false));
 
     return (
       <div id = "interactive-container">
         <h1 id = "header">Quiz: UCLA’s Top 10 Largest Lecture Halls</h1>
-            <p id="trivia-text">
-                Drag and drop the lecture halls below into the correct order by size. 
-                The largest should be at the top. When you have placed all the lecture halls, 
-                a map with the locations of the buildings for the top 10 largest ones will 
-                be displayed. 
-            </p>
-            <Quiz>
-            </Quiz>
+            <div id = "quiz-container">
+              <p id="trivia-text">
+                  Drag and drop the lecture halls below into the correct order by size. 
+                  The largest should be at the top. When you have placed all the lecture halls, 
+                  a map with the locations of the buildings for the top 10 largest ones will 
+                  be displayed. 
+              </p>
+              <Quiz
+                correct = {setCorrect}/>
+            </div>
 
             
 
@@ -43,14 +46,16 @@ const MyMap = () => {
             attribution='&copy <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-          {config.locations.map((loc, index) => {
+            {console.log(correct)}
+          {config.locations.map((loc, index) => { 
             return (<Marker
                 key={loc.name}
                 position={[loc.lat, loc.long]}
+                opacity = { correct[index]? 1: 0}
                 icon={pin}
             >
               <Popup>
-                <p class = "pin-header">{loc.name}</p>
+                <p className = "pin-header">{loc.name}</p>
                 <p>{loc.text}</p>
                 </Popup>
               </Marker>);
